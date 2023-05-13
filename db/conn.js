118c318d2cb9fa5c80efe564af464a8ecee607cb
Akib <akib7599@gmail.com>
2023-05-13 14:50:33 +0530

    romove commit

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,4 @@
  const mongoose = require("mongoose")
-//  mongoose.connect("mongodb://127.0.0.1:27017/dellatecnica_db")
+
  mongoose.connect("mongodb+srv://akib7599:YafieDNY2KyN9aHU@cluster0.hglpxum.mongodb.net/")
 
-// const mongoose = require("mongoose");
-
-// const connectDatabase = async () => {
-  
-//   await mongoose.connect(process.env.MONGODB_URI)
-//   console.log(`MongoDB connected`);
-// };
-
-// module.exports = connectDatabase;
